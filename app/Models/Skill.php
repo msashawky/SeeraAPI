d706954ca27554baca78096b9e6d69c88090c362
”msashawky” <msashawky@gmail.com>
2020-05-08 17:12:49 +0200

    skill fillable

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     public $table = "skills";
-    protected $fillable = ['user_id', 'skill'];
+    protected $fillable = ['user_id', 'skill', 'skill_percentage'];
 
     public function user(){
         return $this->belongsTo('App\Models\User');
